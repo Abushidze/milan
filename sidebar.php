894199c04a4b10eea32dafbcde6fd2eecb090648
Abushidze <unna@academ.org>
2012-12-27 11:32:18 +0700

    сделано фото

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,13 @@
-<div id="sidebar" >
-    <div id="sidebartop" class="sidebar radiusblock group">
-        
+<div id="sidebar">
+    <div id="sidebartop" class="sidebars radiusblock group">
+        <div id="centerphoto">
+            <img id="photo" class="radiusblock"src="<?php bloginfo(template_url) ?>/images/milan.png" alt="мальчик"/>       
+       </div> <!-- centerphoto --> 
     </div><!-- sidebartop -->
-    <div id="sidebarleft" class="sidebar">
+    <div id="sidebarleft" class="sidebars radiusblock">
         
     </div><!-- sidebarleft -->
-    <div id="sidebarright" class="sidebar">
+    <div id="sidebarright" class="sidebars radiusblock">
         
     </div><!-- sidebarright -->
 </div><!-- sidebar --> 
